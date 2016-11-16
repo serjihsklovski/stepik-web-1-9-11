3139e2cc46e2e2f8a3c1d5180fddcf0af7a7f2c0
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-16 22:44:15 +0300

    init

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo rm -rf /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
